b72a2b6bd66888e340a590f7d89ad8bfe0eab780
Jhonatan Lima <jhonatan.vinicius.lima@gmail.com>
2021-04-04 09:52:42 -0300

    Created style of index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,18 @@
+import * as S from '../styles/pages/index'
+
 export default function Home() {
   return (
-    <h1></h1>
+    <S.Container>
+      <section>
+        <span>👏 Hey, welcome! </span>
+        <h1>News about <br /> the <span>React</span> world.</h1>
+        <p>
+          Get access to all the publications <br />
+          <span>for $9.90 month</span>
+        </p>
+      </section>
+
+      <img src="/images/avatar.svg" alt="Girl Coding" />
+    </S.Container>
   )
 }
