e73c87ab6bec21481eac958cbfd571c23e7ac3c1
Jhonatan Lima <jhonatan.vinicius.lima@gmail.com>
2021-07-05 23:55:55 -0300

    created posts page call

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,10 +1,27 @@
+import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
+
 import * as S from './styles'
+
+import { GetStaticProps } from 'next'
+import { getPrismicCLient } from '../../services/prismic'
 
 import {
   SEO
 } from '../../components/index'
 
-export default function Posts() {
+type Post = {
+  slug: string,
+  title: string,
+  excerpt: string,
+  updatedAt: string
+}
+
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <S.Container>
       <SEO
@@ -14,37 +31,48 @@ export default function Posts() {
 
       <S.Main>
         <div>
-          <a href="#">
-            <time>16 de novembro de 2021</time>
-            <strong>Aniversário do cara mais legal do mundo</strong>
-            <p>
-              esse cara é maneiro, porém precisa ter mais foco. Tendo mais foco ele se diverte estudando
-              e fica muito bom naquilo que está fazendo. Ficando muito bom naquilo que está fazendo
-              ele vai ter muito mais chances de ir pra Portugal.
-            </p>
-          </a>
-
-          <a href="#">
-            <time>16 de novembro de 2021</time>
-            <strong>Aniversário do cara mais legal do mundo</strong>
-            <p>
-              esse cara é maneiro, porém precisa ter mais foco. Tendo mais foco ele se diverte estudando
-              e fica muito bom naquilo que está fazendo. Ficando muito bom naquilo que está fazendo
-              ele vai ter muito mais chances de ir pra Portugal.
-            </p>
-          </a>
-
-          <a href="#">
-            <time>16 de novembro de 2021</time>
-            <strong>Aniversário do cara mais legal do mundo</strong>
-            <p>
-              esse cara é maneiro, porém precisa ter mais foco. Tendo mais foco ele se diverte estudando
-              e fica muito bom naquilo que está fazendo. Ficando muito bom naquilo que está fazendo
-              ele vai ter muito mais chances de ir pra Portugal.
-            </p>
-          </a>
+          {posts.map(post => (
+            <a href="#" key={post.slug}>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>
+                {post.excerpt}
+              </p>
+            </a>
+          ))}
         </div>
       </S.Main>
     </S.Container>
   )
+}
+
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicCLient();
+
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'post')
+  ], {
+    fetch: ['post.title', 'post.content'],
+    pageSize: 100,
+  })
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text || '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-Br', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  });
+
+  return {
+    props: {
+      posts,
+    }
+  }
 }
